4540b2b680ec84d1ae73547ab60d8b75c041615a
Damien LaRocque <phicoltan@gmail.com>
2022-01-10 16:13:53 -0500

    Add methods for extracting data + tqdm

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
+"""Rosbag comparator - Compare topics between ROS1 rosbags"""
+
 import json
+import warnings
 from pathlib import Path
 from typing import Union
+
 from rosbags.rosbag1 import Reader
+from tqdm import tqdm
 
 
 class RosbagComparator:
     def __init__(self, path: Union[Path, str]) -> None:
         self._folder = Path(path)
-        self.paths = self._folder.glob("*.bag")
-        self.topics_dict = {}
+        self.paths = [f for f in self._folder.glob("*.bag")]
+        self.topics = {}
+
+    def extract_data(self):
+        """Extract the list of topics from the rosbags"""
+        if len(self.paths) == 0:
+            # Empty list of paths
+            raise RuntimeWarning(f"Specified folder {self.folder} contains no bagfiles")
+
+        if self.topics:
+            # Topics have already been extracted
+            warnings.warn(
+                "Topics are already exported, yet the topics dict will be recreated.",
+                RuntimeWarning,
+            )
+
+        topics = {}
+        with tqdm(total=len(self.paths)) as pbar:
+            for bagfile in self.paths:
+                pbar.set_description(bagfile.stem)
+                topics[bagfile.stem] = self.get_topics(bagfile)
+                pbar.update(1)
+
+        common_set = set().union(*topics.values())
+        differences = {stem: common_set - set(top) for stem, top in topics.items()}
+        differences = {s: list(d) for s, d in differences.items()}
+
+        self.topics = {
+            "topics": topics,
+            "difference": differences,
+            "common": list(common_set),
+        }
 
     @property
     def folder(self):
@@ -17,6 +52,7 @@ class RosbagComparator:
 
     @folder.setter
     def folder(self, value: Union[Path, str]):
+        """Setter for `folder`"""
         if Path(value).is_dir():
             self._folder = value
         else:
@@ -31,12 +67,20 @@ class RosbagComparator:
         return self.paths
 
     def to_json(self, p: Union[Path, str] = None):
+        if not self.topics:
+            warnings.warn(
+                "Topics are not extracted. Use extract_data() before using to_json() instead",
+                RuntimeWarning,
+            )
+            self.extract_data()
+
         p = f"topics_{self.folder.name}.json" if p is None else p
         with open(p, "w") as f:
-            json.dump(self.topics_dict, f)
+            json.dump(self.topics, f)
 
 
 if __name__ == "__main__":
     data_path = Path("data")
     rosbag_comp = RosbagComparator(data_path)
+    rosbag_comp
     rosbag_comp.to_json()
