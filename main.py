61034d7ae72bf33bd525b7121cca58cbed506afb
Damien LaRocque <phicoltan@gmail.com>
2022-01-10 19:04:34 -0500

    Docstrings and remove paths from the attributes

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ from tqdm import tqdm
 class RosbagComparator:
     def __init__(self, path: Union[Path, str]) -> None:
         self._folder = Path(path)
-        self.paths = list(self.folder.glob("*.bag"))
         self.topics = {}
 
     def extract_data(self):
-        """Extract the list of topics from the rosbags"""
-        if len(self.paths) == 0:
+        """Extract all the topics cointaned in the rosbags at the path {self.folder}"""
+        paths = list(self.folder.glob("*.bag"))
+
+        if len(paths) == 0:
             # Empty list of paths
             raise RuntimeWarning(f"Specified folder {self.folder} contains no bagfiles")
 
@@ -28,13 +29,17 @@ class RosbagComparator:
                 RuntimeWarning,
             )
 
+        # Create a dictionary with the list of topics for each bag file
+        # {file1: ["/topic1", ...], ...}
         topics = {}
-        with tqdm(total=len(self.paths)) as pbar:
-            for bagfile in self.paths:
+        with tqdm(total=len(paths)) as pbar:
+            for bagfile in paths:
                 pbar.set_description(bagfile.stem)
                 topics[bagfile.stem] = self.get_topics(bagfile)
                 pbar.update(1)
 
+        # Make a set with all the topics and get the missing topics
+        # for each file
         common_set = set().union(*topics.values())
         differences = {stem: common_set - set(top) for stem, top in topics.items()}
         differences = {s: list(d) for s, d in differences.items()}
@@ -60,11 +65,24 @@ class RosbagComparator:
 
     @staticmethod
     def get_topics(filename: Union[Path, str]):
-        """Get topics from a filename"""
+        """Get a list of the topics in a rosbag file
+
+        Args:
+            filename (Union[Path, str]): path of the rosbag file
+
+        Returns:
+            [type]: list of the topics contained in the rosbag file
+        """
         with Reader(filename) as bag:
             return list(bag.topics.keys())
 
     def to_json(self, p: Union[Path, str] = None):
+        """Export topics dictionary to a json file
+
+        Args:
+            p (Union[Path, str], optional): path of the json file.
+            If None, the topics will be saved in topics_<foldername>.json.
+        """
         if not self.topics:
             warnings.warn(
                 "Topics are not extracted."
